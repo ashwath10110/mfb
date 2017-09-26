e1b5cbcb792ddd5c57b6a990cd992299531b1b46
gs-ashwath <abharadwaj@gainsight.com>
2017-09-27 00:22:19 +0530

    my changes

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,7 +3,61 @@ const router = express.Router();
 
 /* GET api listing. */
 router.get('/', (req, res) => {
-  res.send('api works');
+	res.send('api works');
+});
+
+function initialiseItems() {
+	var items = {
+		'exotic-vegetables': [],
+		'leafy-green-vegetables': [],
+		'fresh-fruits': [],
+		'fresh-vegetables': []
+	};
+
+	var itemCateg = [
+		'exotic-vegetables',
+		'leafy-green-vegetables',
+		'fresh-fruits',
+		'fresh-vegetables'
+	];
+
+
+	for (var i = 0; i < itemCateg.length; i++) {
+			let nameToAppend = '';
+			for (var j = 1; j <= 3; j++) {
+				nameToAppend = itemCateg[i];
+
+				items[nameToAppend].push({
+					id: j + nameToAppend,
+					typeCategory: nameToAppend,
+					type: i,
+					name: j + nameToAppend,
+					summary: nameToAppend + ' A summary will also be present And it will be about.' + j,
+					price: (100 + i) * 10
+				});
+			}
+		}
+	return items;
+}
+
+/* GET Items. */
+router.get('/items', (req, res) => {
+
+	// var ans = initialiseItems();
+
+	// var response_ = {
+	// 	status: true,
+	// 	statusCode: 200,
+	// 	errorMessage: [],
+	// 	data: JSON.stringify(initialiseItems()),
+	// };
+
+	res.send({
+		status: true,
+		statusCode: 200,
+		errorMessage: [],
+		data: JSON.stringify(initialiseItems()),
+	});
 });
 
 module.exports = router;
